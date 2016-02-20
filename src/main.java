a366a1adc33ed17545aa8839bba6e597e0e67ccd
lnvtphu <lnvtphu@gmail.com>
2016-02-20 16:00:01 +0700

    test java and github

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileInputStream in =  null;
+		FileOutputStream out = null;
+		try {
+			
+		} finally {
+			// TODO: handle finally clause
+		}
+
+	}
+
+}
